5273a4a6fbb08595ecb59ee04d2e0fd204ceb4c6
Jae Yong Kim <kim900180@gmail.com>
2018-08-01 01:28:27 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,5 @@
 class Book
+  attr_accessor :author, :page_count, :genre
   def initialize(title)
     @title = title
   end
@@ -7,24 +8,8 @@ class Book
     @title
   end
 
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(num)
-    @page_count = num
-  end
-
   def page_count
     @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
   end
 
   def genre
